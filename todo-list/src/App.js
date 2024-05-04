74e219786bd4807618345b2743eaf8277e9cac57
Likitha13-2001 <133627499+Likitha13-2001@users.noreply.github.com>
2024-05-04 11:35:50 +0530

    Add files via upload

---
+++ todo-list/src/App.js
@@ -0,0 +1,25 @@
+
+import Todo from "./Todo";
+  const App = ()=>{
+    return(
+        <>
+        <Todo/>
+        </>
+    );
+};
+export default App;
+// App.js
+// import React from 'react';
+// import Todo from './Todo'; // Assuming Todo component is in Todo.js
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Todo List</h1>
+//       <Todo />
+//     </div>
+//   );
+// }
+
+// export default App;
+
