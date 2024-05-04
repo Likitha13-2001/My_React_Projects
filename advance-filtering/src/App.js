d46157a244327de5f4ed1db7a4f6929948f10d34
Likitha13-2001 <133627499+Likitha13-2001@users.noreply.github.com>
2024-05-04 11:35:02 +0530

    Add files via upload

---
+++ advance-filtering/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import products from "./db/data";
+import Card from "./components/Card";
+
+function App() {
+  const [selectedCategory,setSelectedCategory] = useState(null);
+
+  const [query,setQuery] = useState("");
+
+  const handleInputChange = event =>{
+    setQuery(event.target.value);
+  };
+
+  const filtereditems=products.filter((product) => 
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !==-1
+  );
+//Radio filter
+
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  //Buttons filter
+
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  function filteredData(products,selected,query){
+    let filteredProducts = products;
+    
+    if (query){
+      filteredProducts = filtereditems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products,selectedCategory,query)
+
+  return(
+    <>
+       <Sidebar handleChange={handleChange}/>
+       <Navigation query={query} handleInputChange={handleInputChange}/>
+       <Recommended handleClick={handleClick}/>
+       <Products result={result}/>
+       
+    </>
+  );
+
+}
+export default App;
